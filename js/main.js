bec2cbfa5860b06c49e81981541e6023f20f84be
Murjejk <97478131+Murjejk@users.noreply.github.com>
2025-09-11 18:58:12 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,9 +34,6 @@ loginBtn.addEventListener("click", () => {
     });
 });
 
-// Google Sheet
-const API_URL = "https://script.google.com/macros/s/AKfycbwAbdw8V5QgEKYGt95VNKJEy0v-bWOl772Aos1HN_Tx3gpdq75WXWsQm6YR4IXB8YGe/exec";
-
 // ==============================
 // Navigering mellan sektioner
 // ==============================
